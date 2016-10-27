8c96136c262dc846f92555088579210dd9cfa272
Covallis-Sustainability-Coalition <CorvallisCoalition@gmail.com>
2016-10-26 19:50:23 -0700

    Update database_cred.php

--- App/Database/database_cred.php
+++ App/Database/database_cred.php
@@ -1,7 +1,7 @@
 <?php
 /* Login credentials for initializing mysqli */
-$DBUrl = "localhost";
-$DBUser = "root";
-$DBPw = "tucu11YB";
-$DBName = "ReuseApp";
+$DBUrl = "";
+$DBUser = "";
+$DBPw = "";
+$DBName = "";
 ?>
