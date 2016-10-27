cada09389679b3955ed112089813dc19b6307c47
Lauren Miller <millerla@onid.oregonstate.edu>
2016-10-27 14:17:07 -0700

    fixed update to v7.AppCompat

--- CRRD/obj/Debug/android/src/mono/MonoPackageManager.java
+++ CRRD/obj/Debug/android/src/mono/MonoPackageManager.java
@@ -99,7 +99,15 @@ class MonoPackageManager_Resources {
 	public static final String[] Assemblies = new String[]{
 		/* We need to ensure that "CRRD.dll" comes first in this list. */
 		"CRRD.dll",
+		"Xamarin.Android.Support.Animated.Vector.Drawable.dll",
+		"Xamarin.Android.Support.Compat.dll",
+		"Xamarin.Android.Support.Core.UI.dll",
+		"Xamarin.Android.Support.Core.Utils.dll",
+		"Xamarin.Android.Support.Fragment.dll",
+		"Xamarin.Android.Support.Media.Compat.dll",
 		"Xamarin.Android.Support.v4.dll",
+		"Xamarin.Android.Support.v7.AppCompat.dll",
+		"Xamarin.Android.Support.Vector.Drawable.dll",
 		"Xamarin.GooglePlayServices.Base.dll",
 		"Xamarin.GooglePlayServices.Basement.dll",
 		"Xamarin.GooglePlayServices.Location.dll",
