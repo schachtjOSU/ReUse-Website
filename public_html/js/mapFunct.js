e5eb7b4e013ea8f7a2f35fba75b9d4b323389f2f
Lauren Miller <millerla@onid.oregonstate.edu>
2016-11-04 17:53:42 -0700

    adding initGeneralMap infowindow

--- public_html/js/mapFunct.js
+++ public_html/js/mapFunct.js
@@ -4,7 +4,7 @@ function initGeneralMap() {
 	//centering the map on Corvallis
 	var Corvallis = {lat: 44.569949, lng: -123.278285};
 	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 12,
+		zoom: 11,
 		center: Corvallis
 	});
 	
@@ -21,16 +21,25 @@ function initGeneralMap() {
 				var myLatLng = {lat: parseFloat(businesses[i].latitude), lng: parseFloat(businesses[i].longitude)};
 				var marker = new google.maps.Marker({
 				position: myLatLng,
-				map: map
+				map: map,
+				title: businesses[i].name,
+				street_address: businesses[i].address_line_1,
+				city_address: businesses[i].city + " " + ", " + businesses[i].abbreviation + " " + businesses[i].zip_code
 				});	
+
+				var infowindow = new google.maps.InfoWindow();
+				
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow.open(map, this);
+					infowindow.setContent("<p><strong>" + this.title + "</strong></p><p>" + this.street_address + "<br>" + this.city_address + "</p>"); 
+				});
+				
 			}
 		}
-		else {
-			
-		}
+		
 	};
 	
-	req.open("GET", "http://localhost/Corvallis-Sustainability-ReUse/public_html/index.php/business", true);//NOTE THAT THIS URL WILL NEED TO BE CHANGED FOR LIVE SITE
+	req.open("GET", "http://localhost/business", true);//NOTE THAT THIS URL WILL NEED TO BE CHANGED FOR LIVE SITE
 	req.send();
 
 }
