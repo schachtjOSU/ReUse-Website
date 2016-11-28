a8e1adf135f7993f83ce333e87e4b5cdc732833f
ccmxy <windrainicefire@gmail.com>
2016-11-28 11:58:40 -0800

    renamed component folders

--- public_html/AdminSite2/components/gridComponent.php
+++ public_html/AdminSite2/components/gridComponent.php
@@ -3,7 +3,7 @@ lists. The list items MUST be added the the ul element (#thisList) below for
 it to work.  -->
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-<link href="components/css/gridComponent.css" rel="stylesheet">
+<link href="components/css-comp-css/gridComponent.css" rel="stylesheet">
 <span class="toggler active" data-toggle="grid"><span class="entypo-layout"></span></span>
 <span class="toggler" data-toggle="list"><span class="entypo-list"></span></span>
 <ul class="list-elements grid" id="thisList">
