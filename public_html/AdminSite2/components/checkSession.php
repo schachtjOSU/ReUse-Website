6841a89dae69cfd746c92153cd033b9ec1bc8390
ccmxy <windrainicefire@gmail.com>
2016-11-30 09:45:10 -0800

    REmoved excess files

--- public_html/AdminSite2/components/checkSession.php
+++ public_html/AdminSite2/components/checkSession.php
@@ -3,10 +3,5 @@
 
 	if(!(isset($_SESSION['username'])) || $_SESSION['username'] == "") {
     header( 'Location: /AdminSite2/loginPage.php' ) ;
-    // echo "<script>window.location.href = webURL + 'loginPage.php';</script>";
    	}
-   	else{
-   		echo 0;
-   	}
-
 ?>
