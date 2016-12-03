df208dfdeebd52aa22dd101ecff65d5e7bf58cf2
ccmxy <windrainicefire@gmail.com>
2016-12-02 16:50:28 -0800

    Succesfully added item

--- public_html/AdminSite2/js/addItemPage.js
+++ public_html/AdminSite2/js/addItemPage.js
@@ -1,56 +1,32 @@
 
 
-/**/
-// function fillSquare(name, category_id, id){
- $("#thisList").append("\
-    <li class='white-square' id='" +"thisTable" + "'> \
-      <span class='box-name'>\
-       <input name='name' type='text' id='name'>\
-       <button class='btn btn-primary' id='add'>add</button>\
-      </span> \
-      <div class='pull-right'> \
-        <span class='below-line-container'>\
-          <span class='below-line'>\
-          <span class='lower-left-corner'>\
-             \
-           <div class='whenDisabled' style='margin-right: 20%;'>\
-               <label id='cat' for='" + ''+ "'>Category id: </label>  \
-               <input name='cat' class='catId' type='text'>\
-           </div>\
-           </span> \
-          </span> \
-        </span> \
-      </div>\
-    </li>");
 
-    setaddEditListener(id, name);
 
 //This will hold the function to add
 var thisaddFunction;
 
 /*Function to set the event listeners*/
-setaddEditListener = function(id, name){
+setAddButtonListener = function(){
  var addButton = document.getElementById('add');
- makeaddFunction(addButton, id, name);
+ makeaddFunction(addButton);
  addButton.addEventListener('click', thisaddFunction, false);
 }
 
 /*Function to set a memebr of the clickListenersForSquares[] array
 to a particular function*/
-makeaddFunction = function(addButton, id, name){
+makeaddFunction = function(addButton){
 
    thisaddFunction = function(){
      var newName = $('#name').val();
      var cat = $('#cat').val();
 
      payload = {};
-     payload.oldName = name;
      payload.name = newName;
      payload.cat = cat;
 
      $.ajax({
          type: "POST",
-         url: "/RUapi/changeItem",
+         url: "/RUapi/items",
          data: payload,
          dataType: 'json',
          success: function(data) {
@@ -61,3 +37,28 @@ makeaddFunction = function(addButton, id, name){
  }; //End of thisaddFunction defintion
 
 }
+
+
+$("#thisList").append("\
+   <li class='white-square' id='" +"thisTable" + "'> \
+     <span class='box-name'>\
+      <input name='name' type='text' id='name'>\
+      <button class='btn btn-primary' id='add'>add</button>\
+     </span> \
+     <div class='pull-right'> \
+       <span class='below-line-container'>\
+         <span class='below-line'>\
+         <span class='lower-left-corner'>\
+            \
+          <div class='whenDisabled' style='margin-right: 20%;'>\
+              <label id='cat' for='" + ''+ "'>Category id: </label>  \
+              <input name='cat' class='catId' type='text'>\
+          </div>\
+          </span> \
+         </span> \
+       </span> \
+     </div>\
+   </li>");
+
+   setAddButtonListener();
+ 
